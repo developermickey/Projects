9a94b4373771f9c1fe13af4eb0d559bba91f6eab
mickeysrishti <Mukeshpathak345@gmail.com>
2025-07-08 08:46:44 +0530

    Task Creation Done

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const closeBtn = document.querySelector("#close-icon");
 const form = document.querySelector(".form");
 const inputTask = document.querySelector("#inputTask");
 const cards = document.querySelector(".cards");
-
-
+const priorityColors = document.querySelectorAll(".priorityColor");
+let modalPriorityColor = 'lightpink'
 
 
 var addBtnFlag = !addBtnFlag;
@@ -27,20 +27,24 @@ closeBtn.addEventListener("click", (() => {
 
 }))
 
-form.addEventListener("keydown", ((e) => {
-    
-    if(e.key == 'Shift'){
+form.addEventListener("submit", ((e) => {
+    e.preventDefault();
+    if(inputTask.value == ''){
+            alert("Please enter your task")
+    }
+
         let id = shortid();
         let task = inputTask.value;
-        taskCreated(task, id)
+        taskCreated(task, id, modalPriorityColor)
         inputTask.value = ""
-    }
 }))
 
 
-function taskCreated(ticketTask, ticketId) {
+function taskCreated(ticketTask, ticketId, ticketColor) {
     const card = document.createElement("div");
     card.setAttribute("class","card")
+    card.setAttribute("style", `border-top: 8px solid ${ticketColor};`)
+
     card.innerHTML = `
                     <div class="card-top">
                         <span class="card-id">${ticketId}</span>
@@ -54,3 +58,17 @@ function taskCreated(ticketTask, ticketId) {
     cards.appendChild(card);
     
 }
+
+
+
+
+priorityColors.forEach((priorityColor) => {
+   priorityColor.addEventListener("click", (() => {
+        priorityColors.forEach((priorityColor) => {
+            priorityColor.classList.remove("active");
+        })
+
+      priorityColor.classList.add("active");
+      modalPriorityColor = priorityColor.classList[0]
+   }))
+})
